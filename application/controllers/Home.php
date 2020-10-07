9496565284c4e206b2a8fbaa0a97ba2ac09e493f
dodhy <dodhyk29@gmail.com>
2020-10-07 20:14:36 +0700

    template

---
+++ application/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+    class Home extends CI_Controller{
+        public function __construct(){
+            parent::__construct();
+        }
+
+        public function index(){
+            $this->load->view('template/header');
+            $this->load->view('template/sidebar');
+            $this->load->view('template/footer');
+        }
+    }
+?>

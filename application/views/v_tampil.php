373e9182882d8a073dcf1137d72a248c801df353
razak357 <sinyosinyo335@gmail.com>
2020-10-14 15:26:57 +0700

    Template Hasilpesan

--- application/views/v_tampil.php
+++ application/views/v_tampil.php
@@ -5,36 +5,53 @@
 </head>
 <body>
 	
-	<table style="margin:20px auto;" border="1">
+	<!-- DataTales Example -->
+	<div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">Tabel Pemesanan</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+<thead>
 		<tr>
 			<th>No</th>
+			<th>Pemesanan</th>
 			<th>Pelanggan</th>
-			<th>Kelas</th>
-			<th>Jumlah Kursi</th>
-            <th>Tanggal Pesan</th>
-            <th>Tanggal Berangkat</th>
-            <th>Tanggal Pulang</th>
-            <th>Alamat Penjemputan</th>
-            <th>Rute</th>
+			<th>Nama</th>
+			<th>Mobil</th>
+            <th>kelas</th>
+            <th>rute</th>
+            <th>Kode Pesan</th>
+            <th>Jumlah</th>
+			<th>Harga Total</th>
+			<th>Tanggal Pesan</th>
+			<th>Tanggal Berangkat</th>
+			<th>Alamat Jemput</th>
 		</tr>
+</thead>
 		<?php 
 		$no = 1;
 		foreach($pemesanan as $p){ 
 		?>
 		<tr>
 			<td><?php echo $no++ ?></td>
-			<td><?php echo $p->pelanggan ?></td>
-			<td><?php echo $p->kelas ?></td>
-            <td><?php echo $p->jumlahkursi ?></td>
-            <td><?php echo $p->tanggalpesan ?></td>
-			<td><?php echo $p->tanggalberangkat ?></td>
-            <td><?php echo $p->tanggalpulang ?></td>
-            <td><?php echo $p->alamatpenjemputan ?></td>
-            <td><?php echo $p->rute ?></td>
+			<td><?php echo $p->id_pemesanan ?></td>
+			<td><?php echo $p->id_pelanggan ?></td>
+			<td><?php echo $p->nama_pelanggan ?></td>
+            <td><?php echo $p->id_mobil ?></td>
+            <td><?php echo $p->id_kelas ?></td>
+			<td><?php echo $p->id_rute ?></td>
+            <td><?php echo $p->kode_pesan ?></td>
+            <td><?php echo $p->jumlah ?></td>
+			<td><?php echo $p->harga_total ?></td>
+			<td><?php echo $p->tanggal_pesan ?></td>
+			<td><?php echo $p->tanggal_berangkat ?></td>
+			<td><?php echo $p->alamat_jemput ?></td>
 			
 		</tr>
 		<?php } ?>
-	</table>
+	
 </body>
 </html>
 
